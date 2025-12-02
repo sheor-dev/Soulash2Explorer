06c50d3386f7e7b4ccbfab87423b7e845fcc23f2
Robyn <rnixon98@gmail.com>
2025-12-02 09:26:16 -0600

    docs: add license statement to new files

--- src/Soulash2Explorer/SkillTag.cs
+++ src/Soulash2Explorer/SkillTag.cs
@@ -1,3 +1,12 @@
+/*
+  Copyright (C) 2025, S2E Contributors
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published 
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+*/
+
 using System;
 using System.Collections.Frozen;
 using System.Collections.Generic;
